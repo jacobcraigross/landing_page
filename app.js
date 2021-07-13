574e81e74d4ac8322c0dc4f258144609ea1ca829
Jacob Ross <JacobRoss@Jacobs-MBP.attlocal.net>
2021-07-13 08:03:21 -0500

    test

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ import './App.css';
 import Button from './components/Button';
 import Search from './components/Search';
 
+
+
 function App() {
   return (
     <div className="App">
+
+    
+      
       <Button/>
       <br/>
+      
       <br/>
       <Search />
     </div>
